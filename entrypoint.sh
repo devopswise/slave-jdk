d614b2d324ab830c05e5735ce7599053af206bad
devopswise <30629983+devopswise@users.noreply.github.com>
2018-07-08 12:51:52 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,25 @@ set -e
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
+write_key() {
+        mkdir -p "${JENKINS_AGENT_HOME}/.ssh"
+        echo "$1" > "${JENKINS_AGENT_HOME}/.ssh/authorized_keys"
+        chown -Rf user:user "${JENKINS_AGENT_HOME}/.ssh"
+        chmod 0700 -R "${JENKINS_AGENT_HOME}/.ssh"
+}
+
+if [[ $JENKINS_SLAVE_SSH_PUBKEY == ssh-* ]]; then
+  write_key "${JENKINS_SLAVE_SSH_PUBKEY}"
+fi
+if [[ $# -gt 0 ]]; then
+  if [[ $1 == ssh-* ]]; then
+    write_key "$1"
+    shift 1
+  else
+    exec "$@"
+  fi
+fi
+
 if ! grep -Fq "${USER_ID}" /etc/passwd; then
     # current user is an arbitrary 
     # user (its uid is not in the 
